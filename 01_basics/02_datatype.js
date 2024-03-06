17e3bc9e5ec5d9d29c7957344ba9374ae4efcedd
swechha613 <110976915+swechha613@users.noreply.github.com>
2024-03-06 06:36:55 +0000

    config done

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -4,3 +4,18 @@ alert(7+7)// we are using nodejs, not browser
 */
 
 console.log(3+3); console.log("swechha")//code readability should be high, always write readable code, this is not readable code.
+
+//number=> 2 to power 53
+//bigint
+//string=>""
+//boolean(yes or no, true or false)
+//null(is a standalone value) you can assgin as value. so null means empty
+//undefined=> means value not assgin 
+//symbol=> mostly use for uniqueness
+//-----------------------------------------------------//
+
+//object
+console.log(typeof "swechha");
+console.log(typeof null);// output->object
+console.log(typeof undefined);// output->undefined
+console.log(typeof 5);
