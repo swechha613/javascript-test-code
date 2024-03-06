213858a52d78dd64d6b0d3abe02a437054114ae1
swechha613 <110976915+swechha613@users.noreply.github.com>
2024-03-06 06:18:29 +0000

    config done

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,6 @@
+"use strict";// treat all js code as newer version
+/*alert("hello")
+alert(7+7)// we are using nodejs, not browser
+*/
+
+console.log(3+3); console.log("swechha")//code readability should be high, always write readable code, this is not readable code.
